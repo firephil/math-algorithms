5fb2d631462f7e9060e525aa5e9e97b35b396183
firephil <philip@firesoft.gr>
2023-01-08 06:04:46 +0200

    Fixed indentations problems

--- factorial/Factorial.scala
+++ factorial/Factorial.scala
@@ -1,81 +1,65 @@
 import scala.annotation.tailrec
 import java.math.MathContext
 
-object Factorial 
- 
+object Factorial{
   
-     def recursive(n : Int) : Int = 
-	  
-	   if(n==0) 1
-	   else n * recursive(n-1)
-	   
-	 
-	 def tailRecursive(n : Int) : Int = 
-	  
-	   @tailrec
-	   def aux(n : Int, acc :Int) :Int = 
-	    
-	     
-		  if(n == 0) acc
-		  else aux(n-1, acc * n)
-	     
-	   aux(n,1)	   
-	   
-	 def tailRecBig(n : BigInt) : BigInt = 
-	  
-	   @tailrec
-	   def aux(n : BigInt, acc :BigInt) :BigInt = 
-	    
-	     
-		  if(n == 0) acc
-		  else aux(n-1, acc * n)
-	     
-	   aux(n,1)	   
-	   
-	 
-	 def functional(n : Int) :Int =  (1 to n).reduce(_ * _)
-	   
-	 
-	 def functionalFoldLeft(n : Int) :Int =   (1 to n).foldLeft(1)(_ * _)
-	    
-	  
-	 def functionalFoldRight(n : Int) :Int =  (1 to n).foldRight(1)(_ * _)
-	    
-	 
-	 
-	 def imperative (n : Int) : Int =  
-	   
-	    var res = 1
-	    var x = 1
-	    
-	    while(x <= n)
-	    
-	     res *= x 
-	     x += 1
+    def recursive(n : Int) : Int =
+        if(n==0) 1
+            else n * recursive(n-1)
+      
 
-	    res
-	   
-	  def imperativeBig (n : BigInt) : BigInt =  
-	   
-	    var res = BigInt(1)
-	    var x = BigInt(1)
-	    
-	    while(x <= n)
-	    
-	     res *= x 
-	     x += 1
-	     
-	     
-	    res
+    def tailRecursive(n : Int) : Int =
+        @tailrec
+        def aux(n : Int, acc :Int) :Int =
+            if(n == 0) acc
+                else aux(n-1, acc * n)
+          
+        aux(n,1)	   
+       
+
+    def tailRecBig(n : BigInt) : BigInt =
+        @tailrec
+        def aux(n : BigInt, acc :BigInt) :BigInt =   
+            if(n == 0) acc
+                else aux(n-1, acc * n)
+            
+        aux(n,1)	   
+        
+
+    def functional(n : Int) :Int = (1 to n).reduce(_ * _)
+ 
+    def functionalFoldLeft(n : Int) :Int = (1 to n).foldLeft(1)(_ * _)
+     
+    def functionalFoldRight(n : Int) :Int = (1 to n).foldRight(1)(_ * _)
+     
+    def imperative (n : Int) : Int =
+        var res = 1
+        var x = 1
+        while(x <= n)  
+            res *= x 
+            x += 1
+        res
+
+    def imperativeBig (n : BigInt) : BigInt =
+        
+        var res = BigInt(1)
+        var x = BigInt(1)
+        
+        while(x <= n)  
+            res *= x 
+            x += 1
+        res
+       
+}
 
 @main
 def run() =
-        println("Factorial of 10")
-        println("Recursive: " + Factorial.recursive(10))
-        println("Tail Recursive: " + Factorial.tailRecursive(10))
-        println("Functional: " + Factorial.functional(10))
-        println("Functional Fold Left: " + Factorial.functionalFoldLeft(10))
-        println("Functional Fold Right: " + Factorial.functionalFoldRight(10))
-        println("Imperative: " + Factorial.imperative(10))
-        println("Tail Recursive Big: " + Factorial.tailRecBig(10))
-        println("Imperative Big: " + Factorial.imperativeBig(10))
+    println("Factorial of 10")
+    println("Recursive: " + Factorial.recursive(10))
+    println("Tail Recursive: " + Factorial.tailRecursive(10))
+    println("Functional: " + Factorial.functional(10))
+    println("Functional Fold Left: " + Factorial.functionalFoldLeft(10))
+    println("Functional Fold Right: " + Factorial.functionalFoldRight(10))
+    println("Imperative: " + Factorial.imperative(10))
+    println("Tail Recursive Big: " + Factorial.tailRecBig(10))
+    println("Imperative Big: " + Factorial.imperativeBig(10))
